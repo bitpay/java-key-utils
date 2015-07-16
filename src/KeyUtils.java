3a34b6eaaf33739d3c006b46b7cca279c38a1f00
Suraj Masand <surajmasand@Surajs-iMac.local>
2015-07-16 12:31:37 -0400

    Updated formatting, tabs

--- src/KeyUtils.java
+++ src/KeyUtils.java
@@ -24,55 +24,55 @@ import com.google.bitcoin.core.Sha256Hash;
 import com.google.bitcoin.core.Utils;
 
 public class KeyUtils {
-	
+
 	// used in private methods at bottom of this file
 	final private static char[] hexArray = "0123456789abcdef".toCharArray();
-	
+
 	public static String generatePem() throws IOException, NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException {
-	
+
 		ECParameterSpec ecSpec = ECNamedCurveTable.getParameterSpec("secp256k1");
 		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
 		KeyPairGenerator kpGenerator = KeyPairGenerator.getInstance("ECDSA", "BC");
 		kpGenerator.initialize(ecSpec, new SecureRandom());
 		KeyPair pair = kpGenerator.generateKeyPair();  // format of keys = PKCS#8, keys are in hex
-		
+
 		StringWriter strWriter = new StringWriter();
 		JcaPEMWriter pemWriter = new JcaPEMWriter(strWriter);
 		pemWriter.writeObject(pair);
 		pemWriter.close();
-		
+
 		String pem = strWriter.toString();
 		return pem;
 	}
-	
+
 	public static String getCompressPubKeyFromPem(String pem) throws IOException {
-		
+
 		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
 		JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
-		
+
 		StringReader strReader = new StringReader(pem);
 		PEMParser pemParser = new PEMParser(strReader);
 		Object keyObject = pemParser.readObject();
 		pemParser.close();
-		
+
 		KeyPair keys = converter.getKeyPair((PEMKeyPair) keyObject);
-		
+
 		String fullPublicKey = keys.getPublic().toString();
 		int indexX = fullPublicKey.indexOf("X: ") + 3; 
 		int indexY = fullPublicKey.indexOf("Y: ") + 3; 
-		
+
 		String xCoord = fullPublicKey.substring(indexX, indexX + 64);
 		xCoord = xCoord.replaceAll("\n", "").replaceAll(" ", ""); // remove unnecessary whitespace 
 		String xCoord64 = checkHas64(xCoord); // make sure x-coordinate is 64 chars long
-		
+
 		// only need the end of the y-coordinate
 		String yCoordEnd = fullPublicKey.substring(indexY+60).toUpperCase();
 		yCoordEnd = yCoordEnd.replaceAll("\n", "").replaceAll(" ", ""); // remove unnecessary whitespace
-		
+
 		// Only need final digit of Y-coordinate to check if even or odd
 		// for prefix to compressed public key
 		Long yFinalNumber = Long.parseLong(yCoordEnd.substring(yCoordEnd.length()-1), 16);
-		
+
 		String prefix;
 		if (yFinalNumber % 2 == 1) {
 			prefix = "03"; // put 03 prefix if y is odd
@@ -80,32 +80,32 @@ public class KeyUtils {
 		else {
 			prefix = "02"; // put 02 prefix if y is even
 		}
-		
+
 		return prefix + xCoord64.toUpperCase();
-		
+
 	}
-	
+
 	public static String getPrivateKeyFromPem(String pem) throws IOException {
 
 		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
 		JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
-		
+
 		StringReader strReader = new StringReader(pem);
 		PEMParser pemParse = new PEMParser(strReader);
 		Object obj = pemParse.readObject();
 		pemParse.close();
-		
+
 		KeyPair kp = converter.getKeyPair((PEMKeyPair) obj);
-		
+
 		String fullPriKey = kp.getPrivate().toString();
 		int startKey = fullPriKey.indexOf("S: ") + 3;
 		String privateKey = fullPriKey.substring(startKey, startKey + 64);
-		
+
 		return privateKey.toUpperCase();
 	}
-	
+
 	public static String getSinFromPem(String pem) throws NoSuchAlgorithmException, IOException {
-		
+
 		String hexPubKey = getCompressPubKeyFromPem(pem);	
 
 		// Step 1 and 2
@@ -113,105 +113,105 @@ public class KeyUtils {
 		byte[] bytesPubKey1 = hexToBytes(hexPubKey);
 		byte[] bytesPubKey2 = Utils.sha256hash160(bytesPubKey1);
 		String shaAndRipe = bytesToHex(bytesPubKey2);
-		
+
 		// Step 3
 		// Add 0F02 to Hex of Step 2
 		String step3 = "0F02" + shaAndRipe;
-		
+
 		// Step 4
 		// Hex to Bytes, Double SHA-256
 		byte[] bytesPubKey3 = hexToBytes(step3);
 		byte[] bytesPubKey4 = Utils.doubleDigest(bytesPubKey3);
-		
+
 		// Step 5
 		// Substring of first 4 bytes (first 8 chars)
 		String step4Hex = bytesToHex(bytesPubKey4);
 		String step5 = step4Hex.substring(0, 8);
-		
+
 		// Step 6
 		// Combine step 3 and step 5
 		String step6 = step3 + step5;
-		
+
 		// Convert to base 58
 		byte[] step6bytes = hexToBytes(step6);
 		String sin = Base58.encode(step6bytes);
-		
+
 		return sin;
 	}
-	
-	
+
+
 	public static String signMsgWithPem(String msg, String pem) throws IOException {
 		String pubKey = getCompressPubKeyFromPem(pem);
 		byte[] pubKeyBytes = hexToBytes(pubKey);
 		String privKey = getPrivateKeyFromPem(pem);
-		
+
 		ECKey key = new ECKey(new BigInteger(privKey, 16), pubKeyBytes, true);
-		
+
 		return sign(key, msg);
 	}
-	
+
 	private static String sign(ECKey key, String input) {
-        byte[] data = input.getBytes();
+		byte[] data = input.getBytes();
 
-        Sha256Hash hash = Sha256Hash.create(data);
-        ECDSASignature sig = key.sign(hash, null);
+		Sha256Hash hash = Sha256Hash.create(data);
+		ECDSASignature sig = key.sign(hash, null);
 
-        byte[] bytes = sig.encodeToDER();
+		byte[] bytes = sig.encodeToDER();
 
-        return bytesToHex(bytes);
-    }
-	
+		return bytesToHex(bytes);
+	}
+
 	// *******************************************
 	// *         Private Methods Section         *
 	// *******************************************
-	
+
 	private static int getHexVal(char hex)
-    {
-        int val = (int)hex;
-        return val - (val < 58 ? 48 : (val < 97 ? 55 : 87));
-    }
-	
-	
+	{
+		int val = (int)hex;
+		return val - (val < 58 ? 48 : (val < 97 ? 55 : 87));
+	}
+
+
 	public static byte[] hexToBytes(String hex) throws IllegalArgumentException
-    {
-        char[] hexArray = hex.toCharArray();
-        
-        if (hex.length() % 2 == 1) {
-            throw new IllegalArgumentException("Error: The binary key cannot have an odd number of digits");
-        }
+	{
+		char[] hexArray = hex.toCharArray();
 
-        byte[] arr = new byte[hex.length() >> 1];
+		if (hex.length() % 2 == 1) {
+			throw new IllegalArgumentException("Error: The binary key cannot have an odd number of digits");
+		}
 
-        for (int i = 0; i < hex.length() >> 1; ++i) {
-            arr[i] = (byte)((getHexVal(hexArray[i << 1]) << 4) + (getHexVal(hexArray[(i << 1) + 1])));
-        }
+		byte[] arr = new byte[hex.length() >> 1];
 
-        return arr;
-    }
+		for (int i = 0; i < hex.length() >> 1; ++i) {
+			arr[i] = (byte)((getHexVal(hexArray[i << 1]) << 4) + (getHexVal(hexArray[(i << 1) + 1])));
+		}
+
+		return arr;
+	}
 
 	private static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
+		char[] hexChars = new char[bytes.length * 2];
 
-        for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
+		for (int j = 0; j < bytes.length; j++) {
+			int v = bytes[j] & 0xFF;
 
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-        }
+			hexChars[j * 2] = hexArray[v >>> 4];
+			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+		}
 
-        return new String(hexChars);
-    }
-	
+		return new String(hexChars);
+	}
+
 	private static String checkHas64(String str) {
 		String str2 = str;
 		if (str2.length() % 2 == 1 && str2.length() < 64)
 			str2 = "0" + str2;
 		while (str2.length() < 64)
 			str2 = "00" + str2;
-		
+
 		return str2;
 	}
-	
+
 
 }
 
--- src/KeyUtilsTest.java
+++ src/KeyUtilsTest.java
@@ -12,106 +12,106 @@ import org.junit.Test;
 import com.google.bitcoin.core.ECKey;
 
 public class KeyUtilsTest {
-	
-		final String pemPattern = "-----BEGIN EC PRIVATE KEY-----\nMHQCA.*SuBBAAK\noUQDQ.*\n.*\n.*END EC PRIVATE KEY-----\n"; 
-		
-		final String pem = "-----BEGIN EC PRIVATE KEY-----\nMHQCAQEEICg7E4NN53YkaWuAwpoqjfAofjzKI7Jq1f532dX+0O6QoAcGBSuBBAAK\noUQDQgAEjZcNa6Kdz6GQwXcUD9iJ+t1tJZCx7hpqBuJV2/IrQBfue8jh8H7Q/4vX\nfAArmNMaGotTpjdnymWlMfszzXJhlw==\n-----END EC PRIVATE KEY-----\n";
-		final String pubKeyCompressed = "038D970D6BA29DCFA190C177140FD889FADD6D2590B1EE1A6A06E255DBF22B4017";
-		final String privateKey = "283B13834DE77624696B80C29A2A8DF0287E3CCA23B26AD5FE77D9D5FED0EE90";
-		final String sin = "TeyN4LPrXiG5t2yuSamKqP3ynVk3F52iHrX";
-		final String msg = "This is a test message.";
-		
-		final String pem2 = "-----BEGIN EC PRIVATE KEY-----\nMHQCAQEEIGxJZ/H8cJ6rTejjSL/jg+bmvV6P1bh+oQyElxQfztbqoAcGBSuBBAAK\noUQDQgAEAJiwjbqNtzFoL1HvdRRWQgzEDMwN08PJC2bdqTmGHmZ0FfX5t+pOy4Ai\nOuluV4VbIFWZ64onGHXu0A7ejWY/jg==\n-----END EC PRIVATE KEY-----\n";
-		final String pubKeyCompressed2 = "020098B08DBA8DB731682F51EF751456420CC40CCC0DD3C3C90B66DDA939861E66";
-		final String privateKey2 = "6C4967F1FC709EAB4DE8E348BFE383E6E6BD5E8FD5B87EA10C8497141FCED6EA";
-		final String sin2 = "Tf3goZsaKmYcD5YWGv2bz2yifbrM3ogkbMd";
-		final String msg2 = "A 2nd test message.";
-		
-		@Test
-		public void testGeneratePem() throws IOException, NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException {
-			String testPem = KeyUtils.generatePem();
-			assertTrue(testPem.matches(pemPattern));
-		}
-		
-		@Test
-		public void testCompressPubKeyFromPem() throws IOException {
-			String pubKey = KeyUtils.getCompressPubKeyFromPem(pem);
-			assertEquals(pubKeyCompressed, pubKey);
-		}
-		
-		
-		@Test
-		public void testPrivKeyFromPem() throws IOException {
-			String priKey = KeyUtils.getPrivateKeyFromPem(pem);
-			assertEquals(privateKey, priKey);			
-		}
-		
-		
-		@Test
-		public void testSinFromPem() throws NoSuchAlgorithmException, IOException {
-			String testSin = KeyUtils.getSinFromPem(pem);
-			assertEquals(sin, testSin);   
-		}
-		
-		
-		@Test
-		public void testSigning() throws IOException, NoSuchAlgorithmException {
-			String hexDer = KeyUtils.signMsgWithPem(msg, pem);
-			MessageDigest md = MessageDigest.getInstance("SHA-256");
-			md.update(msg.getBytes("UTF-8")); 
-			byte[] msgBytes = md.digest();
-						    
-			byte[] hexDerBytes = KeyUtils.hexToBytes(hexDer);
-			byte[] pubKeyBytes = KeyUtils.hexToBytes(KeyUtils.getCompressPubKeyFromPem(pem));
-			
-			assertTrue(ECKey.verify(msgBytes, hexDerBytes, pubKeyBytes));
-		}
-		
-		// *******************************************
-		// 				2nd set of tests
-		// *******************************************
-		
-		@Test
-		public void testGeneratePem2() throws IOException, NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException {
-			String testPem = KeyUtils.generatePem();
-			assertTrue(testPem.matches(pemPattern));
-		}
-		
-		@Test
-		public void testCompressPubKeyFromPem2() throws IOException {
-			String pubKey = KeyUtils.getCompressPubKeyFromPem(pem2);
-			assertEquals(pubKeyCompressed2, pubKey);
-		}
-		
-		
-		@Test
-		public void testPrivKeyFromPem2() throws IOException {
-			String priKey = KeyUtils.getPrivateKeyFromPem(pem2);
-			assertEquals(privateKey2, priKey);			
-		}
-		
-		
-		@Test
-		public void testSinFromPem2() throws NoSuchAlgorithmException, IOException {
-			String testSin = KeyUtils.getSinFromPem(pem2);
-			assertEquals(sin2, testSin);   
-		}
-		
-		
-		@Test
-		public void testSigning2() throws IOException, NoSuchAlgorithmException {
-			String hexDer = KeyUtils.signMsgWithPem(msg2, pem2);
-			MessageDigest md = MessageDigest.getInstance("SHA-256");
-			md.update(msg2.getBytes("UTF-8")); 
-			byte[] msgBytes = md.digest();
-						    
-			byte[] hexDerBytes = KeyUtils.hexToBytes(hexDer);
-			byte[] pubKeyBytes = KeyUtils.hexToBytes(KeyUtils.getCompressPubKeyFromPem(pem2));
-			
-			assertTrue(ECKey.verify(msgBytes, hexDerBytes, pubKeyBytes));
-		}
-		
+
+	final String pemPattern = "-----BEGIN EC PRIVATE KEY-----\nMHQCA.*SuBBAAK\noUQDQ.*\n.*\n.*END EC PRIVATE KEY-----\n"; 
+
+	final String pem = "-----BEGIN EC PRIVATE KEY-----\nMHQCAQEEICg7E4NN53YkaWuAwpoqjfAofjzKI7Jq1f532dX+0O6QoAcGBSuBBAAK\noUQDQgAEjZcNa6Kdz6GQwXcUD9iJ+t1tJZCx7hpqBuJV2/IrQBfue8jh8H7Q/4vX\nfAArmNMaGotTpjdnymWlMfszzXJhlw==\n-----END EC PRIVATE KEY-----\n";
+	final String pubKeyCompressed = "038D970D6BA29DCFA190C177140FD889FADD6D2590B1EE1A6A06E255DBF22B4017";
+	final String privateKey = "283B13834DE77624696B80C29A2A8DF0287E3CCA23B26AD5FE77D9D5FED0EE90";
+	final String sin = "TeyN4LPrXiG5t2yuSamKqP3ynVk3F52iHrX";
+	final String msg = "This is a test message.";
+
+	final String pem2 = "-----BEGIN EC PRIVATE KEY-----\nMHQCAQEEIGxJZ/H8cJ6rTejjSL/jg+bmvV6P1bh+oQyElxQfztbqoAcGBSuBBAAK\noUQDQgAEAJiwjbqNtzFoL1HvdRRWQgzEDMwN08PJC2bdqTmGHmZ0FfX5t+pOy4Ai\nOuluV4VbIFWZ64onGHXu0A7ejWY/jg==\n-----END EC PRIVATE KEY-----\n";
+	final String pubKeyCompressed2 = "020098B08DBA8DB731682F51EF751456420CC40CCC0DD3C3C90B66DDA939861E66";
+	final String privateKey2 = "6C4967F1FC709EAB4DE8E348BFE383E6E6BD5E8FD5B87EA10C8497141FCED6EA";
+	final String sin2 = "Tf3goZsaKmYcD5YWGv2bz2yifbrM3ogkbMd";
+	final String msg2 = "A 2nd test message.";
+
+	@Test
+	public void testGeneratePem() throws IOException, NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException {
+		String testPem = KeyUtils.generatePem();
+		assertTrue(testPem.matches(pemPattern));
+	}
+
+	@Test
+	public void testCompressPubKeyFromPem() throws IOException {
+		String pubKey = KeyUtils.getCompressPubKeyFromPem(pem);
+		assertEquals(pubKeyCompressed, pubKey);
+	}
+
+
+	@Test
+	public void testPrivKeyFromPem() throws IOException {
+		String priKey = KeyUtils.getPrivateKeyFromPem(pem);
+		assertEquals(privateKey, priKey);			
+	}
+
+
+	@Test
+	public void testSinFromPem() throws NoSuchAlgorithmException, IOException {
+		String testSin = KeyUtils.getSinFromPem(pem);
+		assertEquals(sin, testSin);   
+	}
+
+
+	@Test
+	public void testSigning() throws IOException, NoSuchAlgorithmException {
+		String hexDer = KeyUtils.signMsgWithPem(msg, pem);
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		md.update(msg.getBytes("UTF-8")); 
+		byte[] msgBytes = md.digest();
+
+		byte[] hexDerBytes = KeyUtils.hexToBytes(hexDer);
+		byte[] pubKeyBytes = KeyUtils.hexToBytes(KeyUtils.getCompressPubKeyFromPem(pem));
+
+		assertTrue(ECKey.verify(msgBytes, hexDerBytes, pubKeyBytes));
+	}
+
+	// *******************************************
+	// 	2nd set of tests
+	// *******************************************
+
+	@Test
+	public void testGeneratePem2() throws IOException, NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException {
+		String testPem = KeyUtils.generatePem();
+		assertTrue(testPem.matches(pemPattern));
+	}
+
+	@Test
+	public void testCompressPubKeyFromPem2() throws IOException {
+		String pubKey = KeyUtils.getCompressPubKeyFromPem(pem2);
+		assertEquals(pubKeyCompressed2, pubKey);
+	}
+
+
+	@Test
+	public void testPrivKeyFromPem2() throws IOException {
+		String priKey = KeyUtils.getPrivateKeyFromPem(pem2);
+		assertEquals(privateKey2, priKey);			
+	}
+
+
+	@Test
+	public void testSinFromPem2() throws NoSuchAlgorithmException, IOException {
+		String testSin = KeyUtils.getSinFromPem(pem2);
+		assertEquals(sin2, testSin);   
+	}
+
+
+	@Test
+	public void testSigning2() throws IOException, NoSuchAlgorithmException {
+		String hexDer = KeyUtils.signMsgWithPem(msg2, pem2);
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		md.update(msg2.getBytes("UTF-8")); 
+		byte[] msgBytes = md.digest();
+
+		byte[] hexDerBytes = KeyUtils.hexToBytes(hexDer);
+		byte[] pubKeyBytes = KeyUtils.hexToBytes(KeyUtils.getCompressPubKeyFromPem(pem2));
+
+		assertTrue(ECKey.verify(msgBytes, hexDerBytes, pubKeyBytes));
+	}
+
 }
-	
+
 
 
